cd62e0366b1218649981bf4e3f93d07500b7ec76
venomnert <neerthigan@gmail.com>
2023-03-06 08:53:00 -0500

    Determine sentence similarities

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.preprocessing import normalize
-
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -23,8 +23,14 @@ def inference(model_inputs: dict) -> dict:
     sentence_embeddings = model.encode(prompt)
     normalized_embeddings = normalize(sentence_embeddings)
 
+    # Detemine the similarity of the first sentence
+    similarity =cosine_similarity(
+        [sentence_embeddings[0]],
+        sentence_embeddings[1:]
+    )
+
     # Convert the output array to a list
-    output = normalized_embeddings.tolist()
+    output = similarity.tolist()
 
     # Return the results as a dictionary
     return { "data": output }
